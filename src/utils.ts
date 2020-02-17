0579b436826f12ac2503359a1e83e05f60a69aee
Povetkin Dmitriy <povdima19@gmail.com>
2020-02-17 17:53:37 +0200

    fix error handler

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import { ErrorInterface, DestructByKey } from './interfaces';
 import { days } from './constants';
+
 export const decodeErrors = (errors: ErrorInterface[]) => {
   return Array.from(errors || []).reduce((res: object, e: ErrorInterface) => {
     return { ...res, [e.field[0]]: e.messages[0] };
