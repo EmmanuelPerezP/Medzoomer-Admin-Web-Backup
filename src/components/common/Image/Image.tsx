12cd941192f0c47e222d4eeba9cabd5809376035
alexeykosinski <alexey.kosinski@nordwhale.com>
2020-04-10 16:44:16 +0300

    add image width height

--- src/components/common/Image/Image.tsx
+++ src/components/common/Image/Image.tsx
@@ -5,25 +5,38 @@ import logo from '../../../assets/img/terms-logo@3x.png';
 
 import styles from './Image.module.sass';
 
+const prepareSrc = (src: string, width?: number, height?: number) => {
+  if (!src) {
+    return '';
+  }
+  const imageExtension = src.split('.').pop();
+  const imageName = src.split('.').shift();
+  return `${imageName}${width && width > 0 && height && height > 0 ? `_${width}_${height}` : ''}.${imageExtension}`;
+};
+
 export const Image = ({
   className,
   src,
   cognitoId,
   alt,
-  defaultImg
+  defaultImg,
+  width,
+  height
 }: {
   src: string;
   cognitoId: string;
   alt: string;
   className?: string;
   defaultImg?: string;
+  width?: number;
+  height?: number;
 }) => {
   const { getImageLink } = useUser();
   const [image, setImage] = useState('');
 
   const getImage = useCallback(async () => {
     try {
-      const { link } = await getImageLink(cognitoId, src);
+      const { link } = await getImageLink(cognitoId, prepareSrc(src, width, height));
       setImage(link);
     } catch (err) {
       console.error(err);
