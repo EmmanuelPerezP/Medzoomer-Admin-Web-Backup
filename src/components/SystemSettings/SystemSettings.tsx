01196a87a7515dfcc75f170dfdb65653a63d4bb5
Povetkin Dmitriy <povdima19@gmail.com>
2020-06-03 16:59:59 +0300

    add validation for settings

--- src/components/SystemSettings/SystemSettings.tsx
+++ src/components/SystemSettings/SystemSettings.tsx
@@ -5,7 +5,8 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 
 import TextField from '../common/TextField';
 import useSetting from '../../hooks/useSetting';
-import { SETTINGS } from '../../constants';
+import { SETTINGS, settingsError } from '../../constants';
+import Error from '../common/Error';
 
 import styles from './SystemSettings.module.sass';
 
@@ -13,15 +14,59 @@ export const SystemSettings: FC = () => {
   const { getSetting, updateListSettings } = useSetting();
   const [settings, setSettings] = useState();
   const [loading, setLoading] = useState(false);
+  const [err, setErr] = useState({
+    delivery: '',
+    tips: '',
+    default_price_per_delivery_price: '',
+    training_video_link: '',
+    volume_price_per_delivery_offer_per_month: '',
+    volume_price_per_delivery_price: ''
+  });
+
+  const isValid = () => {
+    let isError = true;
+
+    setErr({
+      ...err,
+      ...Object.keys(settings).reduce((res: object, e: any) => {
+        if ((e === 'delivery' || e === 'tips' || e === 'default_price_per_delivery_price') && settings[e] > 100) {
+          isError = false;
+          return { ...res, [e]: `${settingsError[e]} must be lower then 100` };
+        }
+
+        if (e === 'volume_price_per_delivery_offer_per_month' && settings[e] > 1000) {
+          isError = false;
+          return { ...res, [e]: `${settingsError[e]} must be lower then 1000` };
+        }
+
+        if (e !== 'training_video_link' && settings[e] < 0) {
+          isError = false;
+          return { ...res, [e]: `${settingsError[e]} must be greater then 0` };
+        }
+
+        if (!settings[e]) {
+          isError = false;
+          return { ...res, [e]: `${settingsError[e]} is not allowed to be empty` };
+        }
+
+        return { ...res };
+      }, {})
+    });
+
+    return isError;
+  };
 
   const handleUpdateSettings = useCallback(async () => {
-    try {
-      setLoading(true);
-      await updateListSettings(settings);
-      setLoading(false);
-    } catch (e) {
-      setLoading(false);
+    if (isValid()) {
+      try {
+        setLoading(true);
+        await updateListSettings(settings);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+      }
     }
+    return;
   }, [settings]); // eslint-disable-line
 
   useEffect(() => {
@@ -51,8 +96,9 @@ export const SystemSettings: FC = () => {
         ...settings,
         [settingKey]: e.target.value
       });
+      setErr({ ...err, [settingKey]: '' });
     },
-    [settings]
+    [settings, err]
   );
 
   const getSettingValue = useCallback(
@@ -70,79 +116,108 @@ export const SystemSettings: FC = () => {
       <div className={styles.settingBlock}>
         <Typography className={styles.blockTitle}>Courier Commissions</Typography>
         <div className={styles.inputBlock}>
-          <TextField
-            label={'Delivery'}
-            className={styles.procentField}
-            inputProps={{
-              placeholder: 'Delivery',
-              endAdornment: <InputAdornment position="start">%</InputAdornment>
-            }}
-            value={getSettingValue(SETTINGS.COURIER_COMMISSION_DELIVERY)}
-            onChange={handleChangeField(SETTINGS.COURIER_COMMISSION_DELIVERY)}
-          />
-          <TextField
-            label={'Tips'}
-            className={styles.procentField}
-            inputProps={{
-              placeholder: 'Tips',
-              endAdornment: <InputAdornment position="start">%</InputAdornment>
-            }}
-            value={getSettingValue(SETTINGS.COURIER_COMMISSION_TIPS)}
-            onChange={handleChangeField(SETTINGS.COURIER_COMMISSION_TIPS)}
-          />
+          <div>
+            <TextField
+              label={'Delivery'}
+              className={styles.procentField}
+              inputProps={{
+                placeholder: 'Delivery',
+                type: 'number',
+                endAdornment: <InputAdornment position="start">%</InputAdornment>
+              }}
+              value={getSettingValue(SETTINGS.COURIER_COMMISSION_DELIVERY)}
+              onChange={handleChangeField(SETTINGS.COURIER_COMMISSION_DELIVERY)}
+            />
+            {err.delivery ? <Error className={styles.error} value={err.delivery} /> : null}
+          </div>
+          <div>
+            <TextField
+              label={'Tips'}
+              className={styles.procentField}
+              inputProps={{
+                placeholder: 'Tips',
+                type: 'number',
+                endAdornment: <InputAdornment position="start">%</InputAdornment>
+              }}
+              value={getSettingValue(SETTINGS.COURIER_COMMISSION_TIPS)}
+              onChange={handleChangeField(SETTINGS.COURIER_COMMISSION_TIPS)}
+            />
+            {err.tips ? <Error className={styles.error} value={err.tips} /> : null}
+          </div>
         </div>
       </div>
       <div className={styles.settingBlock}>
         <Typography className={styles.blockTitle}>Default Price per Delivery</Typography>
         <div className={styles.inputBlock}>
-          <TextField
-            label={'Price'}
-            className={styles.field}
-            inputProps={{
-              placeholder: 'Price',
-              endAdornment: <InputAdornment position="start">$</InputAdornment>
-            }}
-            value={getSettingValue(SETTINGS.DEFAULT_PRICE_PER_DELIVERY_PRICE)}
-            onChange={handleChangeField(SETTINGS.DEFAULT_PRICE_PER_DELIVERY_PRICE)}
-          />
+          <div>
+            <TextField
+              label={'Price'}
+              className={styles.field}
+              inputProps={{
+                placeholder: 'Price',
+                type: 'number',
+                endAdornment: <InputAdornment position="start">$</InputAdornment>
+              }}
+              value={getSettingValue(SETTINGS.DEFAULT_PRICE_PER_DELIVERY_PRICE)}
+              onChange={handleChangeField(SETTINGS.DEFAULT_PRICE_PER_DELIVERY_PRICE)}
+            />
+            {err.default_price_per_delivery_price ? (
+              <Error className={styles.error} value={err.default_price_per_delivery_price} />
+            ) : null}
+          </div>
         </div>
       </div>
       <div className={styles.settingBlock}>
         <Typography className={styles.blockTitle}>Volume Price per Delivery</Typography>
         <div className={styles.inputBlock}>
-          <TextField
-            label={'Offers per months'}
-            className={styles.field}
-            inputProps={{
-              placeholder: 'Offers per months'
-            }}
-            value={getSettingValue(SETTINGS.VOLUME_PRICE_PER_DELIVERY_OFFER_PER_MONTH)}
-            onChange={handleChangeField(SETTINGS.VOLUME_PRICE_PER_DELIVERY_OFFER_PER_MONTH)}
-          />
-          <TextField
-            label={'Price'}
-            className={styles.field}
-            inputProps={{
-              placeholder: 'Price',
-              endAdornment: <InputAdornment position="start">$</InputAdornment>
-            }}
-            value={getSettingValue(SETTINGS.VOLUME_PRICE_PER_DELIVERY_PRICE)}
-            onChange={handleChangeField(SETTINGS.VOLUME_PRICE_PER_DELIVERY_PRICE)}
-          />
+          <div>
+            <TextField
+              label={'Offers per months'}
+              className={styles.field}
+              inputProps={{
+                type: 'number',
+                placeholder: 'Offers per months'
+              }}
+              value={getSettingValue(SETTINGS.VOLUME_PRICE_PER_DELIVERY_OFFER_PER_MONTH)}
+              onChange={handleChangeField(SETTINGS.VOLUME_PRICE_PER_DELIVERY_OFFER_PER_MONTH)}
+            />
+            {err.volume_price_per_delivery_offer_per_month ? (
+              <Error className={styles.error} value={err.volume_price_per_delivery_offer_per_month} />
+            ) : null}
+          </div>
+          <div>
+            <TextField
+              label={'Price'}
+              className={styles.field}
+              inputProps={{
+                type: 'number',
+                placeholder: 'Price',
+                endAdornment: <InputAdornment position="start">$</InputAdornment>
+              }}
+              value={getSettingValue(SETTINGS.VOLUME_PRICE_PER_DELIVERY_PRICE)}
+              onChange={handleChangeField(SETTINGS.VOLUME_PRICE_PER_DELIVERY_PRICE)}
+            />
+            {err.volume_price_per_delivery_price ? (
+              <Error className={styles.error} value={err.volume_price_per_delivery_price} />
+            ) : null}
+          </div>
         </div>
-      </div>{' '}
+      </div>
       <div className={styles.settingBlock}>
         <Typography className={styles.blockTitle}>Training Video Link</Typography>
         <div className={styles.inputBlock}>
-          <TextField
-            label={'Link'}
-            className={styles.field}
-            inputProps={{
-              placeholder: 'Link'
-            }}
-            value={getSettingValue(SETTINGS.TRAINING_VIDEO_LINK)}
-            onChange={handleChangeField(SETTINGS.TRAINING_VIDEO_LINK)}
-          />
+          <div>
+            <TextField
+              label={'Link'}
+              className={styles.field}
+              inputProps={{
+                placeholder: 'Link'
+              }}
+              value={getSettingValue(SETTINGS.TRAINING_VIDEO_LINK)}
+              onChange={handleChangeField(SETTINGS.TRAINING_VIDEO_LINK)}
+            />
+            {err.training_video_link ? <Error className={styles.error} value={err.training_video_link} /> : null}
+          </div>
         </div>
       </div>
       <div className={styles.navigation}>
