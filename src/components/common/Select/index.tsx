44a6baef74165f22aebb436685ba05a1c448dc93
denis_orda <denis.orda@nordwhale.com>
2021-01-21 16:37:05 +0200

    MED-604 checkboxes in multiple selects

--- src/components/common/Select/index.tsx
+++ src/components/common/Select/index.tsx
@@ -4,6 +4,8 @@ import { createStyles, Theme, withStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select, { SelectProps } from '@material-ui/core/Select';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 import MenuItem from '@material-ui/core/MenuItem';
 import { InputBaseProps } from '@material-ui/core/InputBase';
 import { colors, fontSizes } from '../../../theme';
@@ -23,7 +25,7 @@ export type SelectFieldProps = SelectProps &
   InputBaseProps & {
     id?: string;
     label?: string;
-    value: unknown;
+    value: any;
     items: SelectItem;
     onChange: any;
   };
@@ -43,6 +45,21 @@ const SelectFieldBase: FC<SelectFieldProps & IStyles> = (props) => {
     multiple = false
   } = props;
   const selectId = id || `id-${uuid()}`;
+
+  const getValueTitle = (selected: any) => {
+    if (multiple) {
+      return selected
+        .map((s: string) => {
+          const selectedOne = items.find((item) => item.value === s);
+          return selectedOne ? selectedOne.label : '';
+        })
+        .join(', ');
+    } else {
+      const selectedOne = items.find((item) => item.value === selected);
+      return selectedOne ? selectedOne.label : '';
+    }
+  };
+
   return (
     <FormControl className={classes.root}>
       <InputLabel shrink htmlFor={selectId} classes={{ formControl: classes.selectLabel }}>
@@ -55,6 +72,8 @@ const SelectFieldBase: FC<SelectFieldProps & IStyles> = (props) => {
         IconComponent={IconComponent}
         {...(MenuProps as SelectProps)}
         value={value}
+        // @ts-ignore
+        renderValue={(selected: any) => getValueTitle(selected)}
         input={
           <Input {...(inputProps as InputBaseProps)} classes={{ root: classes.input, input: classes.inputRoot }} />
         }
@@ -64,7 +83,8 @@ const SelectFieldBase: FC<SelectFieldProps & IStyles> = (props) => {
           items.map((item) => {
             return (
               <MenuItem key={item.value} value={item.value}>
-                {item.label}
+                {multiple && <Checkbox checked={value && value.includes(item.value)} />}
+                <ListItemText primary={item.label} />
               </MenuItem>
             );
           })}
