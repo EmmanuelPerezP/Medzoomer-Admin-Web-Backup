03fcdcd3ce357830896d4ffe2a1048b7412f6df1
AlexHodovas <o.hodovas@gmail.com>
2021-05-20 12:09:20 +0300

    linter fix

--- src/components/Pharmacies/components/ReportsTable/ReportsTable.tsx
+++ src/components/Pharmacies/components/ReportsTable/ReportsTable.tsx
@@ -1,6 +1,6 @@
 import { IconButton, Tooltip, Typography } from '@material-ui/core';
 import moment from 'moment';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState, Fragment } from 'react';
 import { useRouteMatch } from 'react-router';
 import usePharmacy from '../../../../hooks/usePharmacy';
 import GridTable from '../../../common/GridTable';
@@ -21,29 +21,31 @@ export const ReportsTable: FC = () => {
     if (id) {
       setLoading(true);
       try {
-        const reports = await getReportsInPharmacy(id);
-        setReports(reports.data);
+        const reportsData = await getReportsInPharmacy(id);
+        setReports(reportsData.data);
         setLoading(false);
       } catch (error) {
-        console.log(error);
+        console.error(error);
         setLoading(false);
       }
     }
   };
 
   useEffect(() => {
-    getReports();
+    void getReports();
     // eslint-disable-next-line
   }, [id]);
 
-  const rows = reports.map((row: any) => [
-    <Typography variant="subtitle2">{moment(row.createdAt).format('MM/DD/YYYY')}</Typography>,
-    <Typography variant="subtitle2">{moment(row.createdAt).format('hh:mm A')}</Typography>,
-    <Tooltip title="Download" placement="top" arrow>
-      <IconButton href={row.url}>
-        <SVGIcon name={'upload'} />
-      </IconButton>
-    </Tooltip>
+  const rows = reports.map((row: any, i) => [
+    <Fragment key={i}>
+      <Typography variant="subtitle2">{moment(row.createdAt).format('MM/DD/YYYY')}</Typography>,
+      <Typography variant="subtitle2">{moment(row.createdAt).format('hh:mm A')}</Typography>,
+      <Tooltip title="Download" placement="top" arrow>
+        <IconButton href={row.url}>
+          <SVGIcon name={'upload'} />
+        </IconButton>
+      </Tooltip>
+    </Fragment>
   ]);
 
   return (
